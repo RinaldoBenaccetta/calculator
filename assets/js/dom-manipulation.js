98a4cd0d396b88a2c33d41e0c468772b166d4a02
Rinaldo Benaccetta <benaccettarinaldo@gmail.com>
2022-08-02 15:16:23 +0200

    add the doc of dom-manipulation.js

--- assets/js/dom-manipulation.js
+++ assets/js/dom-manipulation.js
@@ -1,11 +1,28 @@
+/**
+ * Update the display of the operation in DOM.
+ *
+ * @param {String} newOperation
+ */
 export const refreshOperationDisplay = (newOperation) => {
     updateElement('operation-display', newOperation);
 };
 
+/**
+ * Update the display of the result in DOM.
+ *
+ * @param {String} result
+ */
 export const refreshResultDisplay = (result) => {
     updateElement('result-display', result);
 };
 
+/**
+ * Replace the inner HTML of the element with the provided ID
+ * with the provided Text.
+ *
+ * @param {String} elementId
+ * @param {String} innerHtml
+ */
 const updateElement = (elementId, innerHtml) => {
     const operationDisplay = document.querySelector(`#${elementId}`);
 
